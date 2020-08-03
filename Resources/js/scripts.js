a720449e3844e065e73ca32dde8c3fb893803395
David Frederick <dfrederick79@gmail.com>
2020-08-03 14:17:40 -0500

    Added favicons, jQuery, and additional CSS

---
+++ Resources/js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    //Add/remove sticky navigation bar
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+
+    //Animations on scroll
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset: '50%'
+    });
+
+    //shows/hides nav menu and toggles nav icon
+    $('.js--nav-icon').click(function() {
+        const nav = $('.js--main-nav');
+        const icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')){
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    })
+});
